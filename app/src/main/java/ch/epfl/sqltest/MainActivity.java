731a24e6569ef0aa1f31f94010e42f872991b491
Amro Abdrabo <amro.abdrabo@gmail.com>
2020-03-26 17:00:15 +0100

    friends for later week

--- app/src/main/java/ch/epfl/sqltest/MainActivity.java
+++ app/src/main/java/ch/epfl/sqltest/MainActivity.java
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageSwitcher;
+import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
 
@@ -13,17 +16,23 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements  AsyncResponse {
 
+    EditText a;
+    TextView b;
     public static ChatRepository cr;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        a = findViewById(R.id.editMessage);
+        b = findViewById(R.id.textView);
         final User user1 = new User("amro@abc.com", "amro", "last");
         final User user2 = new User("shaima@abc.com", "shaima", "rashed");
+        final IsFriendsWith friendPair = new IsFriendsWith(user1.email, user2.email);
 
         cr = new ChatRepository(this);
-
         cr.addUser(user1);
         cr.addUser(user2);
-        Chat fromShaimaToAmro = new Chat();
+        cr.addFriend(user1, user2);
+
+        final Chat fromShaimaToAmro = new Chat();
         fromShaimaToAmro.from = user2.email;
         fromShaimaToAmro.to = user1.email;
         cr.addChat(fromShaimaToAmro);
@@ -45,6 +54,15 @@ public class MainActivity extends AppCompatActivity implements  AsyncResponse {
             }
         });
 
+        findViewById(R.id.button_send).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cr.sendMessage(a.getText().toString(), fromShaimaToAmro.chat_id);
+                //cr.getMessages(user1.email, user2.email);
+
+            }
+        });
+
 
     }
 
@@ -52,11 +70,15 @@ public class MainActivity extends AppCompatActivity implements  AsyncResponse {
     @Override
     public void messageFetchFinished(List<String> output) {
 
+
         System.out.println("SENT TO AMROOO:    ");
+        String msg = "";
         for (String el: output)
         {
-            System.out.println(el);
+            msg += el;
+            msg += "\n";
         }
+        b.setText(msg);
 
     }
 }
